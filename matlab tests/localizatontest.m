a772287235c0365a1d6fef264ce7e1acf87eb702
Unknown <grenblot@gmail.com>
2018-05-02 12:14:00 +0200

    dehazing tests

--- matlab tests/localizatontest.m
+++ matlab tests/localizatontest.m
@@ -1,20 +1,18 @@
+clear all
 figure(1)
-imshow('cropped_3.png');
+img = imread('cropped_3.png');
+cropped_img=img(50:500,:);
 rows = sum(img,1);
 [maxval,maxid] = max(rows);
-p1 = [1,maxid];
-p2 = [558,maxid];
-img = imread('cropped_3.png');
+imshow(img);
 hold on
-plot([p1(2),p2(2)],[p1(1),p2(1)],'Color','r','LineWidth',2);
+plot([maxid,maxid],[0,1080],'Color','r','LineWidth',2);
 
 figure(2)
-
 img = imread('cropped_4.png');
+cropped_img=img(50:500,:);
 rows = sum(img,1);
 [maxval,maxid] = max(rows);
-p1 = [1,maxid];
-p2 = [558,maxid];
-imshow('cropped_4.png');
+imshow(img);
 hold on
-plot([p1(2),p2(2)],[p1(1),p2(1)],'Color','r','LineWidth',2);
+plot([maxid,maxid],[0,1080],'Color','r','LineWidth',2);
