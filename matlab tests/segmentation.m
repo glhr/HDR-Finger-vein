4b83d069f1cd26a72bb147696d0220a37b908e0d
Unknown <grenblot@gmail.com>
2018-05-04 10:40:03 +0200

    reconstruction works for arbitrary number of columns

--- matlab tests/segmentation.m
+++ matlab tests/segmentation.m
@@ -1,17 +1,18 @@
 clear all;
 % paper split
 img = imread('4.png');
-%imshow(img, []);
 [nrows, ncols, dim] = size(img);
 % Get the slices colums wise split equally
-divisions_vertical = 4;
-segments1 = [];
+divisions_vertical = 6;
+segments = [];
+reconstructed_img = [];
 figure
 for n = 1:divisions_vertical
     segments(:,:,:,n) = img(:,((n-1)*ncols/divisions_vertical)   +1:n*ncols/divisions_vertical,:);
     subplot(1,divisions_vertical,n), imshow(segments(:,:,:,n),[]);
+    reconstructed_img = horzcat(reconstructed_img, segments(:,:,:,n));
 end
 
-reconstructed_img = [segments(:,:,:,1) segments(:,:,:,2) segments(:,:,:,3) segments(:,:,:,4)];
+
 figure
 imshow(reconstructed_img, []);
