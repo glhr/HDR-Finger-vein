18f618d0f3967953cf78e362640292167a2a15eb
Unknown <grenblot@gmail.com>
2018-05-09 13:11:10 +0200

    detail evaluation gives expected results

--- illumination/segmentation.m
+++ illumination/segmentation.m
@@ -1,7 +1,7 @@
 close all;
 clear all;
 
-showsegments('img_evaltests/dehazed.png',4);
+showsegments('img_evaltests/1.png',4);
 %subtractillumination(imread('img_evaltests/dehazed.png'),1);
 
 function showsegments(imgpath,divisions_vertical)
@@ -9,7 +9,7 @@ function showsegments(imgpath,divisions_vertical)
     %img = img(200:800,150:1500);
     [nrows, ncols, dim] = size(img);
     imgrange = [0 255];
-    plot_nrows = 2;
+    plot_nrows = 4;
 
     segments = [];
     reconstructed_img = [];
@@ -20,13 +20,21 @@ function showsegments(imgpath,divisions_vertical)
         reconstructed_img = horzcat(reconstructed_img, segments(:,:,:,n));
     end
     
-%     for n = 1:divisions_vertical
-%         gradient(segments(:,:,:,n),0);
-%     end
+
     for n = 1:divisions_vertical
-        [segments_processed(:,:,:,n) detail(n)] = subtractillumination(segments(:,:,:,n),0);
-        subplot(plot_nrows,divisions_vertical,n+divisions_vertical),imshow(segments_processed(:,:,:,n),[0 255]),title(sprintf('Detail level: %i', detail(n)));
-    end;
+        [details_weight(:,:,:,n) score_weight(n) details_thresh(:,:,:,n) score_thresh(n)] = subtractillumination(segments(:,:,:,n),0);
+
+        [Gx(:,:,:,n) Gy(:,:,:,n)] = gradient(segments(:,:,:,n),0);
+        
+        subplot(plot_nrows,divisions_vertical,n+divisions_vertical)
+            imshow(details_weight(:,:,:,n),[0 255]),title(sprintf('Detail level: %i', score_weight(n)));
+        subplot(plot_nrows,divisions_vertical,n+2*divisions_vertical)
+            imshow(details_thresh(:,:,:,n),[0 255]),title(sprintf('Detail level: %i', score_thresh(n))); 
+        subplot(plot_nrows,divisions_vertical,n+3*divisions_vertical)
+            imshowpair(Gx(:,:,:,n), Gy(:,:,:,n), 'montage');
+    end
+    
+    
 
 
     %figure
@@ -36,7 +44,7 @@ end   % end of function
 function [Gx, Gy] = gradient(img,plot)
 
     radius = 10;
-    J1 = fspecial('disk', radius);
+    J1 = fspecial('average', radius);
     img_filtered = imfilter(img,J1,'replicate');
     [Gx, Gy] = imgradient(img_filtered,'prewitt');
     %[Gx, Gy] = imgradient(I,'central');
@@ -49,15 +57,22 @@ function [Gx, Gy] = gradient(img,plot)
 
 end
 
-function [output, detail] = subtractillumination(img,plot)
+function [output_weight, detail_weight, output_thresh, detail_thresh] = subtractillumination(img,plot)
     imgneg = uint8(255*mat2gray(imcomplement(img)));
     filter = fspecial('average', 20);
     background_illum = imfilter(imgneg,filter,'replicate');
     output = imsubtract(imgneg,background_illum);
-    output_eq = imadjust(output);
+    output_eq = imadjust(output);    
     output_eq(output_eq < 100) = 0;
-    output = output_eq;
-    detail = sum(output_eq(:));
+    
+    filter2 = fspecial('average', 5);
+    output_filtered = imfilter(output_eq,filter2,'replicate');
+    
+    output_weight = output_eq;
+    detail_weight = sum(output_weight(:));
+    
+    output_thresh = imadjust(uint8(output_filtered>80));
+    detail_thresh = sum(output_thresh(:));
 
     if(plot == 2) % plot everything
         figure;
