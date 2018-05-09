86fc692e681d6706920e3a6b881021665a094749
Unknown <grenblot@gmail.com>
2018-05-09 12:38:58 +0200

    apply detail evaluation to segments

--- illumination/segmentation.m
+++ illumination/segmentation.m
@@ -1,17 +1,16 @@
 close all;
 clear all;
 
-%showsegments('img_evaltests/segment_1.png');
-subtractillumination(imread('img_evaltests/dehazed.png'),1);
+showsegments('img_evaltests/dehazed.png',4);
+%subtractillumination(imread('img_evaltests/dehazed.png'),1);
 
-function showsegments(imgpath)
+function showsegments(imgpath,divisions_vertical)
     img = imread(imgpath);
-    img = img(200:800,150:1500);
+    %img = img(200:800,150:1500);
     [nrows, ncols, dim] = size(img);
     imgrange = [0 255];
-    plot_nrows = 4;
+    plot_nrows = 2;
 
-    divisions_vertical = 4;
     segments = [];
     reconstructed_img = [];
     figure
@@ -20,32 +19,39 @@ function showsegments(imgpath)
         subplot(plot_nrows,divisions_vertical,n), imshow(segments(:,:,:,n),imgrange);
         reconstructed_img = horzcat(reconstructed_img, segments(:,:,:,n));
     end
-
+    
+%     for n = 1:divisions_vertical
+%         gradient(segments(:,:,:,n),0);
+%     end
     for n = 1:divisions_vertical
-         I = segments(:,:,:,n);
-         radius = 10;
-         J1 = fspecial('disk', radius);
-         segments(:,:,:,n) = imfilter(I,J1,'replicate');
-         subplot(plot_nrows,divisions_vertical,n+divisions_vertical),imshow(segments(:,:,:,n),imgrange);
-    end
+        [segments_processed(:,:,:,n) detail(n)] = subtractillumination(segments(:,:,:,n),0);
+        subplot(plot_nrows,divisions_vertical,n+divisions_vertical),imshow(segments_processed(:,:,:,n),[0 255]),title(sprintf('Detail level: %i', detail(n)));
+    end;
 
-    for n = 1:divisions_vertical
-        I = segments(:,:,:,n);
-        [Gx, Gy] = imgradient(I,'prewitt');
-        thresh = Gy>30;
-        subplot(plot_nrows,divisions_vertical,n+2*divisions_vertical),imshowpair(thresh, Gy, 'montage');
-        %[Gx, Gy] = imgradient(I,'central');
-        %subplot(3,divisions_vertical,n),imshowpair(Gx, Gy, 'montage');
-    end
 
     %figure
     %imshow(reconstructed_img,imgrange);
 end   % end of function
 
+function [Gx, Gy] = gradient(img,plot)
+
+    radius = 10;
+    J1 = fspecial('disk', radius);
+    img_filtered = imfilter(img,J1,'replicate');
+    [Gx, Gy] = imgradient(img_filtered,'prewitt');
+    %[Gx, Gy] = imgradient(I,'central');
+    
+    if(plot)
+        figure;
+        subplot(1,2,1),imshow(img_filtered,[0 255]);
+        subplot(1,2,2),imshowpair(Gx, Gy, 'montage');
+    end
+
+end
+
 function [output, detail] = subtractillumination(img,plot)
-    imgneg = imcomplement(img);
+    imgneg = uint8(255*mat2gray(imcomplement(img)));
     filter = fspecial('average', 20);
-    %filter = fspecial('average', 100);
     background_illum = imfilter(imgneg,filter,'replicate');
     output = imsubtract(imgneg,background_illum);
     output_eq = imadjust(output);
