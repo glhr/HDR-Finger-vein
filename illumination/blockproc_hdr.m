21e34c3b8a027dc87d59609410dc27a4d58b6e69
Unknown <grenblot@gmail.com>
2018-05-16 16:27:00 +0200

    compare segmented HDR with global HDR

--- illumination/blockproc_hdr.m
+++ illumination/blockproc_hdr.m
@@ -5,7 +5,7 @@ files = {'img_evaltests/dataset2/segment_cropped (1).png', 'img_evaltests/datase
          'img_evaltests/dataset2/segment_cropped (4).png', 'img_evaltests/dataset2/segment_cropped (5).png'};
 
 %files = {'img_evaltests/dataset2/segment_cropped (1).png', 'img_evaltests/dataset2/segment_cropped (2).png'};
-metafile = {'img_evaltests/dataset2/segment4meta.png','img_evaltests/dataset2/segment4meta.png'};
+metafile = {'img_evaltests/dataset2/segment4meta.png'};
 expTimes = [];
 n_segments = 4;
 images = cell(n_segments,numel(files));
@@ -26,14 +26,36 @@ montage(files)
 
 
 %%plot HDR output for each segments
-figure;
+%figure;
 for i=1:n_segments
     exp_normalized{i} = exposures{i}./exposures{i}(1);
     hdr{i} = makehdr_mod(metafile,images(i,:),'RelativeExposure',exp_normalized{i});
     %figure, imshow(hdr); %was just curious what it looks like
     rgb{i} = tonemap(hdr{i});
-    subplot(1,n_segments,i);
-    imshow(rgb{i});
+    %subplot(1,n_segments,i);
+    %imshow(rgb{i});
+end
+
+figure;
+imshow(reconstructimg(rgb));
+hdr_global();
+
+function hdr_global()
+    files = {'img_evaltests/dataset2/segment_cropped (1).png', 'img_evaltests/dataset2/segment_cropped (2).png', 'img_evaltests/dataset2/segment_cropped (3).png',...  
+         'img_evaltests/dataset2/segment_cropped (4).png', 'img_evaltests/dataset2/segment_cropped (5).png'}; 
+    expTimes = [0.0333, 0.1000, 0.3333, 0.6250, 1.3000]; 
+ 
+    for i = 1:5 
+        path = cell2mat(files(i)); 
+        img = imread(path); 
+      expTimes(i) = mean(img(:)); 
+    end 
+ 
+    %montage(files) 
+    hdr = makehdr(files,'RelativeExposure',expTimes./expTimes(1)); 
+    rgb = tonemap(hdr); 
+    figure; 
+    subplot(2,1,1),imshow(rgb);
 end
 
 function output = segmentimg(img,divisions_vertical)
@@ -43,4 +65,11 @@ function output = segmentimg(img,divisions_vertical)
         output{n} = img(:,((n-1)*floor(ncols/divisions_vertical))   +1:n*floor(ncols/divisions_vertical),:);
     end
     imwrite(output{1},'img_evaltests/dataset2/segment4meta.png');
+end
+
+function output = reconstructimg(segments)
+   output = [];
+   for n = 1:numel(segments)
+       output = horzcat(output, segments{n});
+   end
 end
