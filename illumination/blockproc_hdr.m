e8bca652e61dfbe0f17268856b9237efef92af4a
Unknown <grenblot@gmail.com>
2018-05-16 14:54:55 +0200

    modified makehdr to take arbitrary segments as input instead of filenames

--- illumination/blockproc_hdr.m
+++ illumination/blockproc_hdr.m
@@ -4,15 +4,17 @@ clear all
 files = {'img_evaltests/dataset2/segment_cropped (1).png', 'img_evaltests/dataset2/segment_cropped (2).png', 'img_evaltests/dataset2/segment_cropped (3).png',... 
          'img_evaltests/dataset2/segment_cropped (4).png', 'img_evaltests/dataset2/segment_cropped (5).png'};
 expTimes = [0.0333, 0.1000, 0.3333, 0.6250, 1.3000];
+images = {};
 
 for i = 1:5
     path = cell2mat(files(i));
-    img = imread(path);
+    images{i} = imread(path);
+    img = images{i};
 	expTimes(i) = mean(img(:));
 end
 
 montage(files)
-hdr = makehdr(files,'RelativeExposure',expTimes./expTimes(1));
+hdr = makehdr_mod(files,images,'RelativeExposure',expTimes./expTimes(1));
 figure;
 imshow(hdr)
 rgb = tonemap(hdr);
