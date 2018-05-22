a3f31ad5ad6b66ef84ab8abfa2b1a676b68b6532
Unknown <grenblot@gmail.com>
2018-05-22 10:06:34 +0200

    correct result for uniform relative exposure matrix

--- illumination/global_hdr.m
+++ illumination/global_hdr.m
@@ -8,13 +8,17 @@ files = {'img_evaltests/dataset2/segment_cropped (1).png', 'img_evaltests/datase
          'img_evaltests/dataset2/segment_cropped (4).png', 'img_evaltests/dataset2/segment_cropped (5).png'}; 
     metafile = {'img_evaltests/dataset2/segment4meta.png'};
     %expTimes = [0.0333, 0.1000, 0.3333, 0.6250, 1.3000]; 
-    %expTimes = cell(1,numel(files));
+    expTimes = cell(1,numel(files));
+    expNormalized = cell(1,numel(files));
+    images = cell(1,numel(files));
     for i = 1:5 
         path = cell2mat(files(i)); 
         img = imread(path); 
         images{i} = img;
       %expTimes(i) = mean(img(:)); 
-      expTimes{i} = img;
+      %expTimes{i} = img;
+      expTimes{i}=zeros(size(img));
+      expTimes{i}(:)=mean(img(:)); 
     end 
     
     for i = 1:numel(files) 
