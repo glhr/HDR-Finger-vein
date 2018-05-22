8870793b349734cf2a1f0bbdc041a8c90f53109b
Unknown <grenblot@gmail.com>
2018-05-22 10:46:53 +0200

    vertical segments with different relative exposures

--- illumination/global_hdr.m
+++ illumination/global_hdr.m
@@ -18,7 +18,7 @@ files = {'img_evaltests/dataset2/segment_cropped (1).png', 'img_evaltests/datase
       %expTimes(i) = mean(img(:)); 
       %expTimes{i} = img;
       expTimes{i}=zeros(size(img));
-      expTimes{i}(:)=mean(img(:)); 
+      expTimes{i}(:)= mean(img(:)); 
     end 
     
     for i = 1:numel(files) 
@@ -30,3 +30,31 @@ files = {'img_evaltests/dataset2/segment_cropped (1).png', 'img_evaltests/datase
     rgb = tonemap(hdr); 
     figure; 
     subplot(2,1,1),imshow(rgb);
+    
+    %% compare with segmented approach
+for i = 1:5 
+      path = cell2mat(files(i)); 
+      img = imread(path); 
+
+      expTimes{i} = zeros(size(img));
+      expNormalized{i} = zeros(size(img));
+      expTimes{i}(:) = mean(img(:)); 
+      expNormalized{i} = expTimes{i}./expTimes{i}(1);
+      
+      segwidth = 1;
+      for j = 1:segwidth:302 
+          if(j>=302-segwidth)
+              block = img(:,j:302);
+              expTimes{i}(:,j:302)= mean(block(:)); 
+              expNormalized{i}(:,j:302) = expTimes{i}(:,j:302)./expTimes{i}(1,1);
+          else
+              block = img(:,j:j+segwidth);
+              expTimes{i}(:,j:j+segwidth)= mean(block(:)); 
+              expNormalized{i}(:,j:j+segwidth) = expTimes{i}(:,j:j+segwidth)./expTimes{i}(1,1);
+          end
+      end
+end 
+    
+ hdr = makehdr_mod_cell(metafile,images,'RelativeExposure',expNormalized,'MinimumLimit',exposure_min,'MaximumLimit',exposure_max);
+rgb = tonemap(hdr);  
+subplot(2,1,2),imshow(rgb);
