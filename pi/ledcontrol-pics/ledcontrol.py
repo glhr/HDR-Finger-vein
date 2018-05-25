42e474548a1153b2affe1b43fdd4ff93cdba8bb1
french little shit <grenblot@gmail.com>
2018-05-25 11:26:05 +0200

    loop through different LED illuminations

---
+++ pi/ledcontrol-pics/ledcontrol.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+from picamera import PiCamera
+import os
+import smbus
+import time
+
+
+
+## SETUP LEDS
+bus = smbus.SMBus(1)    # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
+
+DEVICE_ADDRESS = 0x04      #7 bit address (will be left shifted to add the read write bit)
+
+time.sleep(1)
+
+leds = [0, 0, 0, 0, 0, 0, 0, 1]
+
+def ledsoff():
+	bus.write_i2c_block_data(DEVICE_ADDRESS, 16, [0, 0]) #turn off all the LEDs
+	return
+	
+ledsoff()
+
+## SETUP CAMERA
+
+camera =  PiCamera()
+camera.resolution = (800,600)
+#camera.resolution = (1920,1080)
+camera.start_preview()
+camera.zoom = (0.0, 0.0, 1.0, 1.0) 
+camera.exposure_mode = 'off'
+camera.shutter_speed = 20000
+#camera.awb_mode = 'auto'
+#camera.meter_mode = 'spot'
+#camera.shutter_speed = 20000
+#camera.shutter_speed = camera.exposure_speed
+camera.contrast = 90
+camera.brightness = 12
+
+while(max(leds)<1024):
+	leds = [a+10 for a in leds]
+
+	for i in range(len(leds)):
+		#print 'Setting LED ',i+1,' to ', leds[i]
+		pwm = leds[i]
+		byte1 = pwm >> 8
+		byte2 = pwm & 255
+		ledout_bytes = [byte1, byte2]
+		bus.write_i2c_block_data(DEVICE_ADDRESS, i+8, ledout_bytes)
+		time.sleep(0.01)
+		
+	ledstr = [str(a) for a in leds]
+	imgpath = ','.join(ledstr)
+	
+	print '[', imgpath, ']'
+	#camera.capture(imgpath+'.jpg')
+	time.sleep(1)
+	ledsoff()
+
+	# camera.capture(newfolder+'/cam2.'+str(i)+'.jpg')
+
+camera.stop_preview()
+
+
