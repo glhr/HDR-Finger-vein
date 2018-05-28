2e3eab32368e97e59b5f620f253f6ee44191c772
french little shit <grenblot@gmail.com>
2018-05-28 13:36:00 +0200

    added different type of contrast computation

--- illumination/qualitymetrics.m
+++ illumination/qualitymetrics.m
@@ -5,14 +5,15 @@ function output = qualitymetrics(img)
     
     
      output(1) = stats.Contrast;
-     output(2) = mean2(img);
-     output(3) = std2(img);
-     output(4) = entropy(img);
-     output(5) = stats.Energy;
-     output(6) = stats.Homogeneity;
-     output(7) = stats.Correlation;
+     output(2) = contrast(img);
+     output(3) = mean2(img);
+     output(4) = std2(img);
+     output(5) = entropy(img);
+     output(6) = stats.Energy;
+     output(7) = stats.Homogeneity;
+     output(8) = stats.Correlation;
 
-%     output(1) = contrast(img);
+     
 %     output(2) = mean2(img);
 %     output(3) = std2(img);
 %     output(4) = entropy(img);
