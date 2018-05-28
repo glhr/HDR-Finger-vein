febf314886910f0484a4dac76a13bcb3ba0bfd36
french little shit <grenblot@gmail.com>
2018-05-28 14:25:16 +0200

    vertical slider for cropping image

--- illumination/qualitymetrics_gui.m
+++ illumination/qualitymetrics_gui.m
@@ -22,7 +22,7 @@ function varargout = qualitymetrics_gui(varargin)
 
 % Edit the above text to modify the response to help qualitymetrics_gui
 
-% Last Modified by GUIDE v2.5 28-May-2018 12:23:29
+% Last Modified by GUIDE v2.5 28-May-2018 13:38:44
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -51,11 +51,15 @@ function qualitymetrics_gui_OpeningFcn(hObject, eventdata, handles, varargin)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 % varargin   command line arguments to qualitymetrics_gui (see VARARGIN)
-global path1 path2
+global path1 path2 img1 img2 img1_cropped img2_cropped
 path1 = 'img_evaltests/dataset2/segment_cropped (6).png';
 path2 = 'img_evaltests/dataset2/segment_cropped (3).png';
+img1 = imread(path1);
+img2 = imread(path2);
+img1_cropped = img1;
+img2_cropped = img2;
 updategraphs(handles);
-
+set(handles.slider1,'Value',1);
 
 % Choose default command line output for qualitymetrics_gui
 handles.output = hObject;
@@ -111,43 +115,94 @@ else
 end
 
 function updategraphs(handles)
-global path1 path2
+global path1 path2 img1_cropped img2_cropped
 %img1 = imread('img_evaltests/dataset2/segment_cropped (6).png');
 %img2 = imread('img_evaltests/dataset2/segment_cropped (3).png');
-img1 = imread(path1);
-img2 = imread(path2);
+%img1 = imread(path1);
+%img2 = imread(path2);
 
 axes(handles.axes1);
-imshow(img1);
+imshow(img1_cropped);
 axes(handles.axes2);
-imshow(img2);
+imshow(img2_cropped);
 axes(handles.axes3);
-imhist(img1);
+imhist(img1_cropped);
 axes(handles.axes4);
-imhist(img2);
+imhist(img2_cropped);
 
-data(:,1) = qualitymetrics(img1);
-data(:,2) = qualitymetrics(img2);
+data(:,1) = qualitymetrics(img1_cropped);
+data(:,2) = qualitymetrics(img2_cropped);
 
 %# convert matrix of numbers to cell array of strings (right aligned)
 XX = reshape(strtrim(cellstr(num2str(data(:)))), size(data));
 
-%# find cells matching condition
 idx = ( data(:,1) > data(:,2) );
-
-%# use HTML to style these cells
 XX(idx,1) = strcat(...
     '<html><span style="color: #FF0000; font-weight: bold;">', ...
     XX(idx,1), ...
     '</span></html>');
 
-%# find cells matching condition
 idx = ( data(:,2) > data(:,1) );
-
-%# use HTML to style these cells
 XX(idx,2) = strcat(...
     '<html><span style="color: #FF0000; font-weight: bold;">', ...
     XX(idx,2), ...
     '</span></html>');
 
 set(handles.comparisontable, 'Data', XX);
+
+
+% --- Executes on slider movement.
+function slider1_Callback(hObject, eventdata, handles)
+global img1 img1_cropped
+% hObject    handle to slider1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'Value') returns position of slider
+%        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
+
+[h w] = size(img1);
+val = 1 - get(handles.slider1,'Value');
+index = round(val*h);
+if(index<1)
+    index = 1;
+end
+img1_cropped = img1(index:h,:);
+updategraphs(handles);
+
+% --- Executes during object creation, after setting all properties.
+function slider1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to slider1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: slider controls usually have a light gray background.
+if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor',[.9 .9 .9]);
+end
+
+
+
+
+
+% --- Executes on slider movement.
+function slider2_Callback(hObject, eventdata, handles)
+global img2 img1_cropped
+% hObject    handle to slider2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'Value') returns position of slider
+%        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
+
+
+% --- Executes during object creation, after setting all properties.
+function slider2_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to slider2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: slider controls usually have a light gray background.
+if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor',[.9 .9 .9]);
+end
