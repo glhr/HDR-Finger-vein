2ac798e8c5e4cdcd36760a864688452e22a1adf6
french little shit <grenblot@gmail.com>
2018-05-28 12:43:42 +0200

    added metrics for comparison + image browser

--- illumination/qualitymetrics.m
+++ illumination/qualitymetrics.m
@@ -1,9 +1,21 @@
 
 function output = qualitymetrics(img)
-    output(1) = contrast(img);
-    output(2) = mean2(img);
-    output(3) = std2(img);
-    output(4) = entropy(img);
+    glcm = graycomatrix(img);
+    stats = graycoprops(glcm);
+    
+    
+     output(1) = stats.Contrast;
+     output(2) = mean2(img);
+     output(3) = std2(img);
+     output(4) = entropy(img);
+     output(5) = stats.Energy;
+     output(6) = stats.Homogeneity;
+     output(7) = stats.Correlation;
+
+%     output(1) = contrast(img);
+%     output(2) = mean2(img);
+%     output(3) = std2(img);
+%     output(4) = entropy(img);
 end
 
 
--- illumination/qualitymetrics_gui.m
+++ illumination/qualitymetrics_gui.m
@@ -22,7 +22,7 @@ function varargout = qualitymetrics_gui(varargin)
 
 % Edit the above text to modify the response to help qualitymetrics_gui
 
-% Last Modified by GUIDE v2.5 28-May-2018 09:20:55
+% Last Modified by GUIDE v2.5 28-May-2018 12:23:29
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -51,22 +51,10 @@ function qualitymetrics_gui_OpeningFcn(hObject, eventdata, handles, varargin)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 % varargin   command line arguments to qualitymetrics_gui (see VARARGIN)
-
-img1 = imread('img_evaltests/dataset2/segment_cropped (1).png');
-img2 = imread('img_evaltests/dataset2/segment_cropped (3).png');
-
-axes(handles.axes1);
-imshow(img1);
-axes(handles.axes2);
-imshow(img2);
-axes(handles.axes3);
-imhist(img1);
-axes(handles.axes4);
-imhist(img2);
-
-data(:,1) = qualitymetrics(img1);
-data(:,2) = qualitymetrics(img2);
-set(handles.comparisontable, 'Data', data);
+global path1 path2
+path1 = 'img_evaltests/dataset2/segment_cropped (6).png';
+path2 = 'img_evaltests/dataset2/segment_cropped (3).png';
+updategraphs(handles);
 
 
 % Choose default command line output for qualitymetrics_gui
@@ -89,3 +77,55 @@ function varargout = qualitymetrics_gui_OutputFcn(hObject, eventdata, handles)
 % Get default command line output from handles structure
 
 varargout{1} = handles.output;
+
+
+% --- Executes on button press in pushbutton2.
+function pushbutton2_Callback(hObject, eventdata, handles)
+global path2
+% hObject    handle to pushbutton2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+[file,path] = uigetfile('*.png');
+if isequal(file,0)
+   disp('User selected Cancel');
+else
+   path2 = fullfile(path,file)
+   disp(['User selected ', path2]);
+   updategraphs(handles);
+end
+
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+global path1
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+[file,path] = uigetfile('*.png');
+if isequal(file,0)
+   disp('User selected Cancel');
+else
+   path1 = fullfile(path,file)
+   disp(['User selected ', path1]);
+   updategraphs(handles);
+end
+
+function updategraphs(handles)
+global path1 path2
+%img1 = imread('img_evaltests/dataset2/segment_cropped (6).png');
+%img2 = imread('img_evaltests/dataset2/segment_cropped (3).png');
+img1 = imread(path1);
+img2 = imread(path2);
+
+axes(handles.axes1);
+imshow(img1);
+axes(handles.axes2);
+imshow(img2);
+axes(handles.axes3);
+imhist(img1);
+axes(handles.axes4);
+imhist(img2);
+
+data(:,1) = qualitymetrics(img1);
+data(:,2) = qualitymetrics(img2);
+set(handles.comparisontable, 'Data', data);
