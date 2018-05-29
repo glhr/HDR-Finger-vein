a096403c500c0d6d15b6d4c818ee027cdceaaf00
french little shit <grenblot@gmail.com>
2018-05-29 10:13:15 +0200

    import raw image

---
+++ illumination/rawimport.m
@@ -0,0 +1,11 @@
+row=608; col=800;
+fin=fopen('test.raw','r');
+I = fread(fin, col*row*3,'ubit24=>uint32');
+I = reshape(I, col, row, []); 
+B = uint8(bitand(bitshift(I,-00),uint32(255)));
+G = uint8(bitand(bitshift(I,-08),uint32(255)));
+R = uint8(bitand(bitshift(I,-16),uint32(255)));
+I = cat(3,R,G,B);
+Ifinal = flip(imrotate(I, -90),2);
+imagesc(Ifinal);
+fclose(fin);
