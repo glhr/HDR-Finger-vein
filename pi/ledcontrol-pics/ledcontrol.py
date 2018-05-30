cef70bb9f2c9b2a7ca13b78be09c5b304be0972c
french little shit <grenblot@gmail.com>
2018-05-30 12:41:26 +0200

    one folder per image set & write camera settings to txt file

--- pi/ledcontrol-pics/ledcontrol.py
+++ pi/ledcontrol-pics/ledcontrol.py
@@ -24,8 +24,8 @@ ledsoff()
 ## SETUP CAMERA
 
 camera =  PiCamera()
-camera.resolution = (800,600)
-#camera.resolution = (1920,1080)
+#camera.resolution = (800,600)
+camera.resolution = (1920,1080)
 if(enable_preview):
 	camera.start_preview()
 	camera.preview.fullscreen = False
@@ -33,6 +33,7 @@ if(enable_preview):
 camera.exposure_mode = 'off'
 camera.shutter_speed = 1000000
 camera.iso = 100
+camera.shutter_speed = 1000000
 camera.awb_mode = 'auto'
 camera.saturation = -100
 camera.zoom = (0.0, 0.0, 1.0, 1.0) 
@@ -41,6 +42,20 @@ camera.zoom = (0.0, 0.0, 1.0, 1.0)
 #camera.shutter_speed = camera.exposure_speed
 #camera.contrast = 50
 #camera.brightness = 12
+
+folder = time.strftime("%m-%d_%H-%M-%S")
+if not os.path.exists(folder):
+    os.makedirs(folder)
+
+lines =[
+"exposure mode: "+camera.exposure_mode,
+"shutter speed: "+str(camera.shutter_speed),
+"ISO: "+str(camera.iso),
+"AWB mode: "+camera.awb_mode] 
+
+file = open(folder+"/"+folder+"-info","w+")
+file.writelines("%s\n" % l for l in lines)
+file.close()
 
 for dutycyle in range(0,int(1000/(max(ledstrip))),50):
 	leds = [int(a*dutycyle) for a in ledstrip]
@@ -59,7 +74,7 @@ for dutycyle in range(0,int(1000/(max(ledstrip))),50):
 	
 	print '[', imgpath, ']'
 	timestr = time.strftime("%m-%d_%H-%M-%S")
-	camera.capture(timestr+'['+imgpath+'].png')
+	camera.capture(folder+"/"+timestr+'['+imgpath+'].png')
 	#time.sleep(0.05)
 	#
 
