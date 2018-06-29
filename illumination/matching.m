d8d47b9265ff25cd7f222090eab194d8221a1703
french little shit <grenblot@gmail.com>
2018-06-29 03:11:53 +0200

    more automation

--- illumination/matching.m
+++ illumination/matching.m
@@ -1,19 +1,7 @@
 refdataset = 'dataset5';
-inputdataset = 'dataset1';
+inputdataset = 'dataset4';
 
-template = imread(strcat('img_evaltests/dataset5/maxcurve_hdr_[1 5]_movmean_2.8.png'));
-ref1 = imread(strcat('img_evaltests/',refdataset,'/maxcurve_hdr_[1 5]_movmean_2.8.png'));
-ref2 = imread(strcat('img_evaltests/',refdataset,'/maxcurve_hdr_[1 3]_movmean_2.8.png'));
-ref3 = imread(strcat('img_evaltests/',refdataset,'/maxcurve_hdr_[2 5]_movmean_2.8.png'));
-ref4 = imread(strcat('img_evaltests/',refdataset,'/maxcurve_hdr_[1 1]_movmean_2.8.png'));
-ref5 = imread(strcat('img_evaltests/',refdataset,'/maxcurve_hdr_[10 1]_movmean_2.8.png'));
-ref6 = imread(strcat('img_evaltests/',refdataset,'/maxcurve_hdr_[10 10]_movmean_2.8.png'));
+ref = imread(strcat('img_evaltests/dataset4/combinedpattern_3.png'));
+input = imread(strcat('img_evaltests/dataset1/combinedpattern_3.png'));
 
-test = ref1 + ref2 + ref3 + ref4 + ref5 + ref6;
-test = uint8(255*mat2gray(test));
-test = test > 255/6;
-imshow(test);
-
-input = imread(strcat('img_evaltests/',inputdataset,'/maxcurve_hdr_[1 5]_movmean_2.8.png'));
-
-score = miura_match(input, test, 20, 20);
+score = miura_match(input, ref, 20, 20);
--- illumination/recognize.m
+++ illumination/recognize.m
@@ -2,33 +2,37 @@ close all
 clear all
 addpath('code_miura');
 dataset = 'dataset1';
-
+windows = getWindows();
 for i = 2:0.2:3 
     sigma = i;
-    path = 'hdr_[10 1]_movmean';
-    [img, output, pattern] = miura_usage(strcat('img_evaltests/',dataset,'/',path,'.png'),4000,6,9,sigma,1);
-    % figure(1)
-    % subplot(3,1,1)
-    %   imshow(img,[])
-    %   title('Original captured image')
-    % subplot(3,1,2)
-    %   imshow(output)
-    %   title('Maximum curvature method')  
-    % subplot(3,1,3)
-    %   imshow(output)
-    %   title('Repeated line tracking method')
-  
-    imwrite(pattern,strcat('img_evaltests/',dataset,'/maxcurve_',path,'_',num2str(sigma),'.png'));
-  
-    % [img2, maxcurve2, repeatedline2] = miura_usage('cropped_3.png',3000, 1, 21);
-    % subplot(3,2,2)
-    %   imshow(img2,[])
-    %   title('Original captured image')
-    % subplot(3,2,4)
-    %   imshow(maxcurve2)
-    %   title('Maximum curvature method')  
-    % subplot(3,2,6)
-    %   imshow(repeatedline2)
-    %   title('Repeated line tracking method')
+    
+    for j=1:numel(windows)
+        window = mat2str(windows{j});
+        [img, output, pattern] = miura_usage(strcat('img_evaltests/',dataset,'/hdr_',window,'_movmean.png'),4000,6,9,sigma,1);
+        % figure(1)
+        % subplot(3,1,1)
+        %   imshow(img,[])
+        %   title('Original captured image')
+        % subplot(3,1,2)
+        %   imshow(output)
+        %   title('Maximum curvature method')  
+        % subplot(3,1,3)
+        %   imshow(output)
+        %   title('Repeated line tracking method')
+
+        imwrite(pattern,strcat('img_evaltests/',dataset,'/maxcurve_',window,'_',num2str(sigma),'.png'));
+
+        % [img2, maxcurve2, repeatedline2] = miura_usage('cropped_3.png',3000, 1, 21);
+        % subplot(3,2,2)
+        %   imshow(img2,[])
+        %   title('Original captured image')
+        % subplot(3,2,4)
+        %   imshow(maxcurve2)
+        %   title('Maximum curvature method')  
+        % subplot(3,2,6)
+        %   imshow(repeatedline2)
+        %   title('Repeated line tracking method')
+    end
+    combine_veinpatterns(dataset,sigma);
 
 end
