76b7ce2fb1bcb8a295b8e103ef7598c1f2b8be16
french little shit <grenblot@gmail.com>
2018-06-29 02:27:40 +0200

    make plotting optional and loop through different movavg window sizes

--- illumination/global_hdr.m
+++ illumination/global_hdr.m
@@ -7,6 +7,8 @@ dataset = 'dataset5';
 metafile = {strcat('img_evaltests/',dataset,'/segment_cropped (1).png')};
 [img_h,img_w] = size(imread( cell2mat(metafile)));
 
+plot = false;
+
 files = {
         strcat('img_evaltests/',dataset,'/segment_cropped (1).png'), ...
         strcat('img_evaltests/',dataset,'/segment_cropped (2).png'), ...
@@ -52,8 +54,10 @@ expTimes = cell(1,numel(files));
 expNormalized = cell(1,numel(files));
 images = cell(1,numel(files));
 
-figure;
-montage(files,'Size', [NaN 1]);
+if(plot)
+    figure;
+    montage(files,'Size', [NaN 1]);
+end
 
 
 %% single scalar relative exposure value per image 
@@ -72,61 +76,72 @@ for i = 1:numel(files)
 %   expNormalized{i} = expNormalized{i} .* expNormalized{i};
 end 
 
-%montage(files) 
+if(plot)
+    montage(files) 
+end
 hdr_global = makehdr_mod_cell(metafile,images,'RelativeExposure',expNormalized,'MinimumLimit',exposure_min,'MaximumLimit',exposure_max);
 %hdr = makehdr_mod_cell(metafile,images,'RelativeExposure',expNormalized);
 %rgb = localtonemap(hdr,'EnhanceContrast', 1); 
 %hdr_rgb = cat(3, hdr,hdr,hdr);
 rgb_global = tonemap(hdr_global);  
-rgb_global = uint8(255*mat2gray(hdr_global));
-figure; 
-subplot(2,1,1),imshow(rgb_global,[]);
+%rgb_global = uint8(255*mat2gray(hdr_global));
+if(plot)
+    figure; 
+    subplot(2,1,1),imshow(rgb_global,[]);
+end
 imwrite(rgb_global,strcat('img_evaltests/',dataset,'/hdr_global.png'));
 
 %% compare with moving average approach
 
-for i = 1:numel(files) 
-    path = cell2mat(files(i)); 
-    img = imread(path); 
-    %expTimes(i) = mean(img(:)); 
-    %expTimes{i} = img;
-    expTimes{i}=zeros(size(img));
-    expTimes{i}= movmean(img,75,1);
-    expNormalized{i} = expTimes{i}./expTimes{i}(1);
-end 
-
-hdr = makehdr_mod_cell(metafile,images,'RelativeExposure',expNormalized,'MinimumLimit',exposure_min,'MaximumLimit',exposure_max);
-%hdr_rgb = cat(3, hdr,hdr,hdr);
-%rgb = tonemap_mod(hdr);  
-rgb = uint8(255*mat2gray(hdr));
-%subplot(2,1,2),imshow(rgb);
-imwrite(rgb,strcat('img_evaltests/',dataset,'/hdr_75,1.png'));
+% for i = 1:numel(files) 
+%     path = cell2mat(files(i)); 
+%     img = imread(path); 
+%     %expTimes(i) = mean(img(:)); 
+%     %expTimes{i} = img;
+%     expTimes{i}=zeros(size(img));
+%     expTimes{i}= movmean(img,75,1);
+%     expNormalized{i} = expTimes{i}./expTimes{i}(1);
+% end 
+% 
+% hdr = makehdr_mod_cell(metafile,images,'RelativeExposure',expNormalized,'MinimumLimit',exposure_min,'MaximumLimit',exposure_max);
+% %hdr_rgb = cat(3, hdr,hdr,hdr);
+% %rgb = tonemap_mod(hdr);  
+% rgb = uint8(255*mat2gray(hdr));
+% %subplot(2,1,2),imshow(rgb);
+% imwrite(rgb,strcat('img_evaltests/',dataset,'/hdr_75,1.png'));
 
 %% compare with moving average approach
 
-for i = 1:numel(files) 
-    path = cell2mat(files(i)); 
-    img = imread(path); 
-    %expTimes(i) = mean(img(:)); 
-    %expTimes{i} = img;
-    expTimes{i}=zeros(size(img));
-    window = [2 5];
-    expTimes{i}= movmean(img,window);
-    %expTimes{i}= movsum(img,window);
-    expNormalized{i} = expTimes{i}./expTimes{i}(1);
-end 
+windows = {[1 1],[1 2], [1 3], [1 4], [1 5], [2 5], [3 5], [4 5], [5 5], [1 10]};
 
-hdr = makehdr_mod_cell(metafile,images,'RelativeExposure',expNormalized,'MinimumLimit',exposure_min,'MaximumLimit',exposure_max);
-%hdr_rgb = cat(3, hdr,hdr,hdr);
-rgb = tonemap(hdr);  
-%rgb = uint8(255*mat2gray(hdr));
-subplot(2,1,2),imshow(rgb,[]);
-window = mat2str(window);
-imwrite(rgb,strcat('img_evaltests/',dataset,'/hdr_',window,'_movmean.png'));
+for j = 1:numel(windows)
+    for i = 1:numel(files) 
+        path = cell2mat(files(i)); 
+        img = imread(path); 
+        %expTimes(i) = mean(img(:)); 
+        %expTimes{i} = img;
+        expTimes{i}=zeros(size(img));
+        window = windows{j};
+        expTimes{i}= movmean(img,window);
+        %expTimes{i}= movsum(img,window);
+        expNormalized{i} = expTimes{i}./expTimes{i}(1);
+    end 
 
-figure;
-path = cell2mat(files(1)); 
-img = imread(path); 
-adjust = imadjust(img(:,:,1));
-imshow(adjust);
-imwrite(adjust,strcat('img_evaltests/',dataset,'/imadjust_result.png'));
+    hdr = makehdr_mod_cell(metafile,images,'RelativeExposure',expNormalized,'MinimumLimit',exposure_min,'MaximumLimit',exposure_max);
+    %hdr_rgb = cat(3, hdr,hdr,hdr);
+    rgb = tonemap(hdr);  
+    %rgb = uint8(255*mat2gray(hdr));
+    if(plot)
+        subplot(2,1,2),imshow(rgb,[]);
+    end
+    window = mat2str(window);
+    imwrite(rgb,strcat('img_evaltests/',dataset,'/hdr_',window,'_movmean.png'));
+
+end
+
+% figure;
+% path = cell2mat(files(1)); 
+% img = imread(path); 
+% adjust = imadjust(img(:,:,1));
+% imshow(adjust);
+% imwrite(adjust,strcat('img_evaltests/',dataset,'/imadjust_result.png'));
