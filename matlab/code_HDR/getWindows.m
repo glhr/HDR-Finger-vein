8406295b8b24c5f1e6617e21d51b5f3467ec5b69
glhr <grenblot@gmail.com>
2018-12-03 14:08:40 +0200

    added comment

--- matlab/code_HDR/getWindows.m
+++ matlab/code_HDR/getWindows.m
@@ -1,5 +1,5 @@
-# defines the window(s) to be used for the moving mean filter
-# if multiple windows are defined, the HDR algorithm will be run for each of them
+%% defines the window(s) to be used for the moving mean filter
+%% if multiple windows are defined, the HDR algorithm will be run for each of them
 
 function windows = getWindows()
     %windows = {[1 1],[1 2], [2 2], [1 3], [3 3], [4 4], [1 4], [1 5], [2 5], [3 5], [4 5], [5 5], [1 10], [10 10], [1 75], [75 1]};
