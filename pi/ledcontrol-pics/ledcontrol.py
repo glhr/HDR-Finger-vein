9b8c633dc8e7f00d7eaf1bdedf8d47e6f1e2bd8e
glhr <grenblot@gmail.com>
2018-12-03 13:59:28 +0200

    added comments

--- pi/ledcontrol-pics/ledcontrol.py
+++ pi/ledcontrol-pics/ledcontrol.py
@@ -4,23 +4,19 @@ import os
 import smbus
 import time
 
-enable_preview = False
-enable_capture = True
+enable_preview = False # enable this if you want a live preview of the camera during the capture (note:slow)
+enable_capture = True # enable this if you want to save captures to image files
 
 ## SETUP LEDS
 bus = smbus.SMBus(1)    # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
-
 DEVICE_ADDRESS = 0x04      #7 bit address (will be left shifted to add the read write bit)
-
 time.sleep(1)
-
-#[base of finger .... end of finger]
-#ledstrip = [1, 0.3, 0, 0, 1, 0.3, 0, 0.7]
  
 def ledsoff():
 	bus.write_i2c_block_data(DEVICE_ADDRESS, 16, [0, 0]) #turn off all the LEDs
 	return
-	
+
+# takes an array of 8 relative led intensities as input eg. [1, 0.3, 0, 0, 1, 0.3, 0, 0.7] ([base of finger .... end of finger])
 def setleds(leds):
 	for i in range(len(leds)):
 		#print 'Setting LED ',i+1,' to ', leds[i]
@@ -39,13 +35,13 @@ ledsoff()
 ## SETUP CAMERA
 
 camera =  PiCamera()
-camera.resolution = (800,600)
-#camera.resolution = (1920,1080)
+camera.resolution = (800,600) # note: resolution greatly affects capture time
+#camera.resolution = (1920,1080) 
 if(enable_preview):
 	camera.start_preview()
 	camera.preview.fullscreen = False
 	camera.preview.window = (800,0,800,600)
-camera.exposure_mode = 'off'
+camera.exposure_mode = 'off' #disable automatic exposure setting
 camera.shutter_speed = 1000000
 camera.iso = 100
 camera.awb_mode = 'auto'
@@ -57,11 +53,13 @@ camera.zoom = (0.0, 0.0, 1.0, 1.0)
 #camera.contrast = 50
 #camera.brightness = 12
 
+#used for debugging different finger vein scanner configurations
 cap = "yes"
 irfilter = "no"
 fingerwindow = "no"
 extraledstrips = "no"
 
+#ask the user for the finger number (1 to 5) and set the LED pattern accordingly
 finger = str(raw_input("Which finger?"));
 fingerno = int(finger[:1]);
 
@@ -103,6 +101,8 @@ if(enable_capture):
 	file.writelines("%s\n" % l for l in lines)
 	file.close()
 	
+# pwmstep: PWM duty cycle increment between captures
+# pwmmax: maximum PWM duty cycle after which capture is stopped
 if(enable_capture):
 	pwmstep = 50
 	pwmmax = 1024
@@ -110,6 +110,7 @@ else:
 	pwmstep = 10
 	pwmmax = 300
 
+# progressively increase the exposure while maintaining the relative intensities defined in ledstrip
 for dutycyle in range(0,int(2000/(max(ledstrip))),pwmstep):
 	leds = [int(a*dutycyle) for a in ledstrip]
 
@@ -125,7 +126,7 @@ for dutycyle in range(0,int(2000/(max(ledstrip))),pwmstep):
 			camera.capture(folder+"/"+timestr+'['+imgpath+'].png')
 	
 	if(max(leds)>=pwmmax):
-		leds = [1024, 1024, 1024, 1024, 1024, 1024, 1024, 300]
+		leds = [1024, 1024, 1024, 1024, 1024, 1024, 1024, 300] #make sure the PWM duty cycle stays <= 1024. Tip of the finger is set to a lower duty cycle because it is thinner
 		setleds(leds)
 		
 		ledstr = [str(a) for a in leds]
@@ -147,5 +148,3 @@ ledsoff()
 
 if(enable_preview):
 	camera.stop_preview()
-
-
